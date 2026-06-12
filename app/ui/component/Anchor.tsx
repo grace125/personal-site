e2c34b7c96f8445df2c1af3222be866097ac8ee8
Grace Schorno <graceschorno@protonmail.com>
2026-06-12 15:31:39 -0700

    Make hyperlinks not show visited links that are relative

--- app/ui/component/Anchor.tsx
+++ app/ui/component/Anchor.tsx
@@ -4,8 +4,8 @@ import { ReactNode } from "react";
 
 
 
-export default function A(props: { children: ReactNode, href: string, rel?: boolean }) {
-  const color = props.rel 
+export default function A(props: { children: ReactNode, href: string }) {
+  const color = props.href.startsWith("./") || props.href.startsWith("/")
     ? "text-hyperlink hover:text-hyperlink-hover" 
     : "text-hyperlink hover:text-hyperlink-hover visited:text-hyperlink-visited"
   return <Link target="_blank" rel="noopener noreferrer" className={`underline ${color}`} href={props.href}>{props.children}</Link>
