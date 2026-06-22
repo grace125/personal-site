341b1064b213614359e88de72300e7a8a0787ac0
Grace Schorno <graceschorno@protonmail.com>
2026-06-21 17:20:56 -0700

    Redirect from root directory

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  // redirects: () => [
+  //     {
+  //       source: '/',
+  //       destination: '/home',
+  //       permanent: true,
+  //     },
+  // ]
 };
 
 export default nextConfig;
