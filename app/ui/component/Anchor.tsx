8a39003765147926b97c8c3ecfa4c5ea1bfe53dd
Grace Schorno <graceschorno@protonmail.com>
2026-06-21 18:19:02 -0700

    Prevent internal URLs from opening a new tab

--- app/ui/component/Anchor.tsx
+++ app/ui/component/Anchor.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import { ReactNode } from "react";
+import { z, Z } from "@/app/lib/Z"
 
-
-
+const urlSchema = z.url()
 
 export default function A(props: { children: ReactNode, href: string }) {
-  const color = props.href.startsWith("./") || props.href.startsWith("/")
-    ? "text-hyperlink hover:text-hyperlink-hover" 
-    : "text-hyperlink hover:text-hyperlink-hover visited:text-hyperlink-visited"
-  return <Link target="_blank" rel="noopener noreferrer" className={`underline ${color}`} href={props.href}>{props.children}</Link>
+  const isExternalUrl = Z.parse(urlSchema, props.href).isOk()
+  const target = isExternalUrl ? "_blank" : undefined
+  const color = isExternalUrl
+    ? "text-hyperlink hover:text-hyperlink-hover visited:text-hyperlink-visited"
+    : "text-hyperlink hover:text-hyperlink-hover" 
+  return <Link target={target} rel="noopener noreferrer" className={`underline ${color}`} href={props.href}>{props.children}</Link>
 }
