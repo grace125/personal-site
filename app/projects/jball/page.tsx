c0637fd04a2f668c59fd792fcc1d83779e495daa
Grace Schorno <graceschorno@protonmail.com>
2026-07-07 13:08:03 -0700

    Update J-Ball Page

--- app/projects/jball/page.tsx
+++ app/projects/jball/page.tsx
@@ -1,3 +1,4 @@
+import A from "@/app/ui/component/Anchor";
 import { ProjectEntry } from "@/app/ui/component/document/Article";
 import ItchLink from "@/app/ui/component/ItchLink";
 import Section from "@/app/ui/component/sections/Section";
@@ -16,25 +17,26 @@ export default function Page() {
 
             
             <p>
-                I did this for a game jam in my third year of Computer Science, and its main purpose was to prototype a lip-syncing system in Bevy.
-                The system I used was to get a sprite for a closed mouth, an "Ee" sound, an "Oo" sound, and an "Aa" sound. 
-                Then I put a list of timestamps and associated mouth positions in a file, next to the corresponding audio file it transcribed, and loaded both in at the same time.
+                This was one of my earliest game jams. 
+                Its main purpose was to prototype a lip-syncing system in Bevy.
+                Each mouth movement was a closed mouth, an "Ee", an "Oo", or an "Aa".
+                I timestamped these mouth positions in a file next to the corresponding audio file it described, and loaded/played both together.
             </p>
             {/* <P>As it turns out, transcribing lip movements, especially </P> */}
 
-            <Section h="But How Did You Get That File?">
+            <Section h="How I Got that File">
                 <p>
-                    Well, you see, I went in Audacity, zoomed in to super small increments of time, looped that increment about 10-to-30 times each to find when exactly the mouth would have moved, and hand-added that data to the file. 
-                    And my dear friend Justin has a bit of a stutter, so it, took, a <b><i>long</i></b> time.
-                    A full day of time, for one file.
+                    To produce these timestamped files, I went in Audacity, zoomed in to super small increments of time, looped that increment about 10-to-30 times each, and found exactly when the mouth would have changed position, and hand-added that data to the file. 
+                    My dear friend Justin (the voice of J-Ball and the composer for this game) has a bit of a stutter, so this, took, a <b><i>long</i></b> time.
+                    In particular, a full day of time, for one file.
                 </p>
                 <p>
-                    Does this game have more than one level? No. 
-                    Does it use more than one of the many recordings Justin gave me? No. 
-                    Does it have a game over screen, or a pause button? <i>Nope.</i> Does it at least feel good to play? No, not that either.
+                    If I was to do this project over, I would have made a system similar to what <A href="https://www.twitch.tv/tomthinks">Tom Thinks</A> has on Twitch, where each movement of the mouth is controlled <i>as the speaker talks</i>.
+                    This would have saved me leagues of time, and let me work on adding a proper game over state.
                 </p>
-                <p>Where was I going with this?</p>
-                <p>Oh yeah. But is the game funny? Yes. And that's all I really wanted.</p>
+                <p>
+                    Maybe one day the world will see the rest of J-Ball transcribed. Who knows!
+                </p>
             </Section>
         </ProjectEntry>
     </>
